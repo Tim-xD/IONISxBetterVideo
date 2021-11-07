10bed9822fd18f92a4500d4d5240478065cdad69
Tim-xD <74541981+Tim-xD@users.noreply.github.com>
2021-11-07 23:40:17 +0100

    Add files via upload

--- background.js
+++ background.js
@@ -14,30 +14,15 @@ function onError(error) {
 function blockRequest(result) {
     function blockYt() {
         function logActive(activetabs) {
-            function logTabs(tabs) {
-                let instance = result.instance || "invidious.osi.kr";
-                let ionisxtab = false;
-                let activeid = activetabs[0].id;
+            let instance = result.instance || "invidious.osi.kr";
+            let url = activetabs[0].url;
 
-                for (let tab of tabs) {
-                    if (tab.id == activeid) {
-                        ionisxtab = true;
-                    }
-                }
-
-                let instanceYt = instance.includes("youtube")
-                if ((!ionisxtab || (instanceYt && ionisxtab)) && activeid >= 0) {
-                    console.log("!blocked")
-                    return { cancel: false };
-                }
-                else {
-                    console.log("blocked")
-                    return { cancel: true };
-                }
+            if (url.includes("youtube.com") || (instance.includes("youtube.com") && url.includes("courses.ionisx.com"))) {
+                return { cancel: false };
             }
-
-            let querying_ionis = browser.tabs.query({ url: "https://courses.ionisx.com/*" });
-            return querying_ionis.then(logTabs, onError);
+            else {
+                return { cancel: true };
+            }
         }
 
         let querying_active = browser.tabs.query({ currentWindow: true, active: true });
